8d5e7e37344e3fc5b095a1e9236a1f469b24285a
Lucas Easley <lucaseasley11@gmail.com>
2020-04-15 14:08:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_numbers=  []
    array.each do |numbers|
-    puts "#{numbers} * #{numbers}"
+    new_numbers << puts "#{numbers} * #{numbers}"
   end
   return new_numbers
 end
